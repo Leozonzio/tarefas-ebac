37d16672fdc46a5d282ad64e65fcf7821fb8e250
Leozonzio <lixeiraleo@gmail.com>
2022-02-09 19:58:17 -0300

    tarefa média

---
+++ Tarefa calculo médio/src/MediaFodase/NotaAlunos.java
@@ -0,0 +1,14 @@
+package MediaFodase;
+public class NotaAlunos{
+    public static void main(String []args){
+        double aluno1 = 8.4;
+        double aluno2 = 9.1;
+        double aluno3 = 2.3;
+        double aluno4 = 6.8;
+        {
+            double media = aluno1 + aluno2 + aluno3 + aluno4;
+            double mediaTotal = media / 4;
+            System.out.println(mediaTotal);
+        }
+    }
+}
