edf8b0a06393679b45aef3990a31edbdc9eee197
Leozonzio <lixeiraleo@gmail.com>
2022-02-18 15:54:03 -0300

    tarefa_wrappers

---
+++ tarefa wrappers/src/tarefa/TarefaWrappers.java
@@ -0,0 +1,12 @@
+package tarefa;
+
+public class TarefaWrappers {
+
+    public static void main(String[] args) {
+
+        int codigo = 12;
+        Integer codigo2 = codigo;
+        System.out.println(codigo2);
+
+    }
+}
